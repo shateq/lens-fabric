8661dcf433a10c03200eb8ab514e5895193b27c8
Szymon <shateq21@gmail.com>
2022-02-05 18:08:54 +0100

    /fov command :D

--- src/main/java/shateq/mods/fabric/lens/LensClient.java
+++ src/main/java/shateq/mods/fabric/lens/LensClient.java
@@ -1,15 +1,24 @@
 package shateq.mods.fabric.lens;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TranslatableComponent;
 
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_GRAVE_ACCENT;
 
 public class LensClient implements ClientModInitializer {
     public static boolean pauseScreen = false;
+    private final static String command = "fov";
     private static KeyMapping key;
 
     @Override
@@ -23,5 +32,38 @@ public class LensClient implements ClientModInitializer {
                 client.setScreen(new LensScreen());
             }
         });
+
+        ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal(command)
+                .then(ClientCommandManager.argument("number", IntegerArgumentType.integer())
+//                        .suggests((context, builder) -> {
+//                            new IntegerSuggestion(new StringRange(30, 110), 70);
+//                            return builder.buildFuture();
+//                        })
+                    .executes(context -> {
+                        final LocalPlayer p = context.getSource().getPlayer();
+                        int number = IntegerArgumentType.getInteger(context, "number");
+                        number = evalFovNumber(number);
+                        p.displayClientMessage(
+                                new TranslatableComponent("command.lens.fov").append(Component.nullToEmpty(String.valueOf(number)).copy().withStyle(ChatFormatting.GREEN)),
+                                false);
+                        updateFov(number); // Doing a thing
+                        return 0;
+                    })
+                ));
+    }
+
+    public static void updateFov(int number) {
+        Minecraft.getInstance().options.fov = evalFovNumber(number);;
+        Minecraft.getInstance().levelRenderer.needsUpdate();
+    }
+
+    public static int evalFovNumber(int num) {
+        if(num > 110) {
+            return 110;
+        }
+        if(num < 30) {
+            return 30;
+        }
+        return num;
     }
 }
--- src/main/java/shateq/mods/fabric/lens/LensScreen.java
+++ src/main/java/shateq/mods/fabric/lens/LensScreen.java
@@ -8,7 +8,8 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.Option;
 import net.minecraft.client.gui.chat.NarratorChatListener;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import org.jetbrains.annotations.NotNull;
 
 @Environment(EnvType.CLIENT)
 public class LensScreen extends Screen {
@@ -43,14 +44,14 @@ public class LensScreen extends Screen {
         }
     }
 
-    public void render(PoseStack poseStack, int i, int j, float deltaTime) {
+    public void render(@NotNull PoseStack poseStack, int i, int j, float deltaTime) {
         if (!this.checkToClose()) {
             poseStack.pushPose();
             RenderSystem.enableBlend();
 
             poseStack.popPose();
             super.render(poseStack, i, j, deltaTime);
-            drawCenteredString(poseStack, this.font, Component.nullToEmpty("Detailed info about your lens..."), this.width / 2, this.height / 2 + 12, WHITE);
+            drawCenteredString(poseStack, this.font, new TranslatableComponent("gui.lens.text"), this.width / 2, this.height / 2 + 12, WHITE);
             /* Blit-ing, whenever I'll want to add my graphics
             static final ResourceLocation LOCATION = new ResourceLocation("textures/gui/container/gamemode_switcher.png");
             RenderSystem.setShader(GameRenderer::getPositionTexShader);
